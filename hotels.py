563601d6e3f8c30d851bdd749431ac3598c0982d
Ma4etea <alexvlod85@gmail.com>
2025-06-08 17:08:13 +0300

    задание

--- hotels.py
+++ hotels.py
@@ -1,35 +1,71 @@
 from fastapi import Query, Body, Path, APIRouter
 
+from schemas.hotels import Hotel, HotelPatch
+
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 hotels = [
-    {"id": 1, "title": "sochi", "name": "hotel"},
-    {"id": 2, "title": "дубай", "name": "motel"},
+    {"id": 1, "title": "Sochi", "name": "hotel"},
+    {"id": 2, "title": "Dubai", "name": "motel"},
+    {"id": 3, "title": "Paris", "name": "hostel"},
+    {"id": 4, "title": "Tokyo", "name": "ryokan"},
+    {"id": 5, "title": "New York", "name": "apartment"},
+    {"id": 6, "title": "Berlin", "name": "guesthouse"},
+    {"id": 7, "title": "Rome", "name": "bnb"},
+    {"id": 8, "title": "Bangkok", "name": "villa"},
+    {"id": 9, "title": "Barcelona", "name": "resort"},
+    {"id": 10, "title": "Cape Town", "name": "lodge"}
 ]
 
 
 @router.get("")
 def get_hotels(
-    id_: int | None = Query(None, description="ID"),
-    title: str | None = Query(None, description="Название отеля"),
+        id_: int | None = Query(None, description="ID"),
+        title: str | None = Query(None, description="Название отеля"),
+        page: int | None = Query(1, description="какая страницы"),
+        per_page: int | None = Query(3, description="Сколько в странице"),
 ):
+    total = len(hotels)
+    if 0 >= per_page or per_page > total:
+        return {f"per_page must be from 0 to {total}"}
+    total_pages = (total / per_page).__ceil__()
+    if  not 0 < page <= total_pages:
+        return {"status": f"page {page} does not exist"}
+    start = per_page * page - per_page
+    end = per_page * page
     hotels_ = [
         hotel
         for hotel in hotels
         if title
-        and hotel["title"].lower() == title.lower()
-        and id_
-        and hotel["id"] == id_
+           and hotel["title"].lower() == title.lower()
+           and id_
+           and hotel["id"] == id_
     ]
     if not hotels_:
-        return hotels
-    return hotels_
+        return hotels[start:end]
+    return hotels_[start:end]
 
 
 @router.post("")
-def add_hotel(title: str = Body(), name: str | None = Body(None)):
+def add_hotel(
+        hotel_data: Hotel = Body(
+            openapi_examples={
+                "1": {
+                    "summary": "Дубай",
+                    "value": {"title": "Дубай мубай", "name": "sjdhisu"},
+                },
+                "2": {
+                    "summary": "Сочи",
+                    "value": {"title": "Сочи мочи", "name": "sjdhisu"},
+                },
+            }
+        )
+):
     global hotels
-    hotels.routerend({"id": len(hotels) + 1, "title": title, "name": name})
+
+    hotels.append(
+        {"id": len(hotels) + 1, "title": hotel_data.title, "name": hotel_data.name}
+    )
     return {"status": "ok"}
 
 
@@ -44,19 +80,15 @@ def delete_hotel(hotel_id: int = Path()):
 
 
 @router.patch("/{hotel_id}")
-def edit_hotel(
-    hotel_id: int = Path(),
-    title: str | None = Body(None),
-    name: str | None = Body(None),
-):
+def edit_hotel(hotel_id: int, hotel_data: HotelPatch):
     global hotels
     hotels_ = [hotel for hotel in hotels if hotel["id"] == hotel_id]
     hotel = hotels_[0]
     if hotel:
-        if title:
-            hotel["title"] = title
-        if name:
-            hotel["name"] = name
+        if hotel_data.title:
+            hotel["title"] = hotel_data.title
+        if hotel_data.name:
+            hotel["name"] = hotel_data.name
         return [hotel for hotel in hotels if hotel["id"] == hotel_id]
     return {"status": "error"}
 
@@ -66,13 +98,16 @@ def edit_hotel(
     summary="Изменение отеля",
     description="Только полное обновление",
 )
-def upd_hotel(hotel_id: int = Path(), title: str = Body(), name: str = Body()):
+def upd_hotel(
+        hotel_id: int,
+        hotel_data: Hotel,
+):
     global hotels
     hotels_ = [hotel for hotel in hotels if hotel["id"] == hotel_id]
     hotel = hotels_[0]
     print(hotel is hotels[hotel_id - 1])
     if hotel:
-        hotel["title"] = title
-        hotel["name"] = name
+        hotel["title"] = hotel_data.title
+        hotel["name"] = hotel_data.name
         return [hotel for hotel in hotels if hotel["id"] == hotel_id]
     return {"status": "error"}
