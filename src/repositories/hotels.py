4d675c053474f689074b40f27b9cb1bb0ee76b11
Ma4etea <alexvlod85@gmail.com>
2025-06-11 23:16:44 +0300

    put,delete

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,6 @@
-from fastapi import HTTPException
-from sqlalchemy import func, select, Insert, delete, update
-from pydantic import BaseModel as BaseSchema
-from src.database import engine
+
+from sqlalchemy import func, select
+
 from src.models.hotels import HotelsOrm
 from src.repositories.base import BaseRepository
 
@@ -29,42 +28,7 @@ class HotelsRepository(BaseRepository):
         result = await self.session.execute(query)
         return result.scalars().all()
 
-    async def add(self, data: BaseSchema):
-        add_hotel_stmt = Insert(self.model).values(**data.model_dump()).returning(self.model)
-        print(add_hotel_stmt.compile(bind=engine, compile_kwargs={"literal_binds": True}))
-        result = await self.session.execute(add_hotel_stmt)
-        return result.scalars().one()
 
-    async def edit(self, data: BaseSchema, **filter_by):
-
-        query = select(self.model).filter_by(**filter_by)
-        result = await self.session.execute(query)
-        result_orm = result.scalars().all()
-        if len(result_orm) > 1:
-            await self.session.rollback()
-            raise HTTPException(status_code=422)
-        if len(result_orm) < 1:
-            await self.session.rollback()
-            raise HTTPException(status_code=404)
-
-        stmt = update(self.model).filter_by(**filter_by).values(**data.model_dump())
-        print(stmt.compile(bind=engine, compile_kwargs={"literal_binds": True}))
-        await self.session.execute(stmt)
-
-    async def delete(self, **filter_by):
-        query = select(self.model).filter_by(**filter_by)
-        result = await self.session.execute(query)
-        result_orm = result.scalars().all()
-        if len(result_orm) > 1:
-            await self.session.rollback()
-            raise HTTPException(status_code=422)
-        if len(result_orm) < 1:
-            await self.session.rollback()
-            raise HTTPException(status_code=404)
-
-        stmt = delete(self.model).filter_by(**filter_by)
-        print(stmt.compile(bind=engine, compile_kwargs={"literal_binds": True}))
-        await self.session.execute(stmt)
 
 
 
