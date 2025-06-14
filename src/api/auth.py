38806e083c4bebb4b5a14e84948f9361962de758
Ma4etea <alexvlod85@gmail.com>
2025-06-14 19:10:24 +0300

    DBDep

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,9 +1,6 @@
-from fastapi import APIRouter, HTTPException, Response, Request
-from starlette.requests import cookie_parser
+from fastapi import APIRouter, HTTPException, Response
 
-from src.api.dependecy import AccessDep
-from src.database import new_session
-from src.repositories.auth import AuthRepository
+from src.api.dependecy import AccessDep, DBDep
 from src.schemas.users import UserReg, UserAdd
 from src.services.auth import Authservice
 
@@ -11,50 +8,45 @@ router = APIRouter(prefix="/auth", tags=["Авторизация"])
 
 
 @router.post("/register")
-async def register_user(data: UserReg):
+async def register_user(db: DBDep, data: UserReg):
     hashed_password = Authservice().pwd_context.hash(data.password)
     new_data_user = UserAdd(email=data.email, hashed_password=hashed_password)
-    async with new_session() as session:
-        await AuthRepository(session).add(new_data_user)
-        await session.commit()
-        return {"status": "ok"}
+
+    await db.auth.add(new_data_user)
+    await db.commit()
+    return {"status": "ok"}
 
 
 @router.post("/login")
-async def login_user(
-        data: UserReg,
-        response: Response
-):
-    async with new_session() as session:
-        user = await AuthRepository(session).get_user_with_hashed_password(email=data.email)
-        if not user:
-            raise HTTPException(status_code=401, detail="Пользователь не зарегистрирован")
-        if not Authservice().verify_password(plain_password=data.password,
-                                             hashed_password=user.hashed_password):  # new_case проверка пароля
-            raise HTTPException(status_code=401, detail="пароль не верный")
-        access_token = Authservice().create_access_token(user_id=user.id)
+async def login_user(db: DBDep, data: UserReg, response: Response):
+    user = await db.auth.get_user_with_hashed_password(email=data.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Пользователь не зарегистрирован")
+    if not Authservice().verify_password(
+        plain_password=data.password, hashed_password=user.hashed_password
+    ):  # new_case проверка пароля
+        raise HTTPException(status_code=401, detail="пароль не верный")
+    access_token = Authservice().create_access_token(user_id=user.id)
 
-        response.set_cookie("access_token", access_token)  # new_case добавление куки
+    response.set_cookie("access_token", access_token)  # new_case добавление куки
 
-        return {"access_token": access_token}
+    return {"access_token": access_token}
 
 
 @router.get("/me")
-async def get_me(
-        user_id: AccessDep
-):
-    async with new_session() as session:
-        user = await AuthRepository(session).get_one_none(id=user_id)
-        if not user:
-            raise HTTPException(status_code=404)
+async def get_me(db: DBDep, user_id: AccessDep):
+    user = await db.auth.get_one_none(id=user_id)
+    if not user:
+        raise HTTPException(status_code=404)
 
     return user
 
 
 @router.post("/logout")
 async def logout(
-        response: Response,
-        _: AccessDep  # new_case переменная мусорка по конвенции python
+    db: DBDep,
+    response: Response,
+    _: AccessDep,  # new_case переменная мусорка по конвенции python
 ):
     response.delete_cookie("access_token")
     return {"status": "ok"}
--- src/api/rooms.py
+++ src/api/rooms.py
@@ -1,42 +1,38 @@
 from fastapi import APIRouter, Path, HTTPException
-
-from src.api.dependecy import AccessDep
-from src.database import new_session
-from src.repositories.rooms import RoomsRepository
+from src.api.dependecy import AccessDep, DBDep
 from src.schemas.room import AddRoom, AddRoomToDb, EditRoom
 
 router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_rooms(_: AccessDep, hotel_id: int = Path()):
-    async with new_session() as session:
-        return {
-            "status": "OK",
-            "data": await RoomsRepository(session).get_all(hotel_id=hotel_id),
-        }
+async def get_rooms(_: AccessDep, db: DBDep, hotel_id: int = Path()):
+    return {
+        "status": "OK",
+        "data": await db.rooms.get_all(hotel_id=hotel_id),
+    }
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
 async def get_room(
     _: AccessDep,
+    db: DBDep,
     hotel_id: int = Path(),
     room_id: int = Path(),
 ):
-    async with new_session() as session:
-        room = await RoomsRepository(session).get_one_none(hotel_id=hotel_id, id=room_id)
-        if not room:
-            raise HTTPException(404)
-        return {"status": "OK", "data": room}
+    room = await db.rooms.get_one_none(hotel_id=hotel_id, id=room_id)
+    if not room:
+        raise HTTPException(404)
+    return {"status": "OK", "data": room}
 
 
 @router.post("/{hotel_id}/rooms")
-async def add_room(_: AccessDep, room_data: AddRoom, hotel_id: int = Path()):
+async def add_room(_: AccessDep, db: DBDep, room_data: AddRoom, hotel_id: int = Path()):
     new_room_data = AddRoomToDb(**room_data.model_dump(), hotel_id=hotel_id)
 
-    async with new_session() as session:
-        room = await RoomsRepository(session).add(new_room_data)
-        await session.commit()
+    room = await db.rooms.add(new_room_data)
+    await db.commit()
+
     return {
         "status": "OK",
         "data": room,
@@ -46,38 +42,39 @@ async def add_room(_: AccessDep, room_data: AddRoom, hotel_id: int = Path()):
 @router.delete("/{hotel_id}/rooms/{room_id}")
 async def remove_room(
     _: AccessDep,
+    db: DBDep,
     hotel_id: int = Path(),
     room_id: int = Path(),
 ):
-    async with new_session() as session:
-        await RoomsRepository(session).delete(hotel_id=hotel_id, id=room_id)
-        await session.commit()
+    await db.rooms.delete(hotel_id=hotel_id, id=room_id)
+    await db.commit()
+
     return {"status": "OK"}
 
 
 @router.put("/{hotel_id}/rooms/{room_id}")
 async def update_room(
-    room_data: AddRoom,
+    db: DBDep,
     _: AccessDep,
+    room_data: AddRoom,
     hotel_id: int = Path(),
     room_id: int = Path(),
 ):
-    async with new_session() as session:
-        await RoomsRepository(session).edit(room_data, hotel_id=hotel_id, id=room_id)
-        await session.commit()
+    await db.rooms.edit(room_data, hotel_id=hotel_id, id=room_id)
+    await db.commit()
+
     return {"status": "OK"}
 
 
 @router.patch("/{hotel_id}/rooms/{room_id}")
 async def edit_room(
-    room_data: EditRoom,
     _: AccessDep,
+    db: DBDep,
+    room_data: EditRoom,
     hotel_id: int = Path(),
     room_id: int = Path(),
 ):
-    async with new_session() as session:
-        await RoomsRepository(session).edit(
-            room_data, exclude_unset=True, hotel_id=hotel_id, id=room_id
-        )
-        await session.commit()
+    await db.rooms.edit(room_data, exclude_unset=True, hotel_id=hotel_id, id=room_id)
+    await db.commit()
+
     return {"status": "OK"}
