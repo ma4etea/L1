68fbe957d54f82dd9225729139445e24eca9f10f
Ma4etea <alexvlod85@gmail.com>
2025-06-19 10:56:19 +0300

    SQL

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,6 @@
 from datetime import date
 
-from fastapi import HTTPException
-from sqlalchemy import func, select, distinct
+from sqlalchemy import func, select
 
 from src.models.hotels import HotelsOrm
 from src.models.rooms import RoomsOrm
@@ -14,20 +13,30 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_available_hotels(self, title, location, offset, limit, date_from: date, date_to: date):
+    async def get_available_hotels(
+        self, title, location, offset, limit, date_from: date, date_to: date
+    ):
         hotels_ids = select(self.model.id)
 
         if title:
-            hotels_ids = hotels_ids.where(HotelsOrm.title.ilike(f'%{title.strip()}%'))
+            hotels_ids = hotels_ids.where(HotelsOrm.title.ilike(f"%{title.strip()}%"))
 
         if location:
-            hotels_ids = hotels_ids.where(func.lower(HotelsOrm.location).contains(location.strip().lower()))
+            hotels_ids = hotels_ids.where(
+                func.lower(HotelsOrm.location).contains(location.strip().lower())
+            )
 
         rooms = (
-            select(RoomsOrm.hotel_id).distinct()
+            select(RoomsOrm.hotel_id)
+            .distinct()
             .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(
-                get_available_rooms_ids(offset=offset, limit=limit, date_from=date_from, date_to=date_to)))
+            .filter(
+                RoomsOrm.id.in_(
+                    get_available_rooms_ids(
+                        offset=offset, limit=limit, date_from=date_from, date_to=date_to
+                    )
+                )
+            )
         )
 
         hotels_ids = hotels_ids.filter(self.model.id == rooms.c.hotel_id)
@@ -35,9 +44,3 @@ class HotelsRepository(BaseRepository):
         # print(query.compile(compile_kwargs={"literal_binds": True}))
 
         return await self.get_all(None, None, HotelsOrm.id.in_(hotels_ids))
-
-        # raise HTTPException(404)
-        #
-        # result = await self.session.execute(query)
-        # models = result.scalars().all()
-        # return [self.schema.model_validate(model, from_attributes=True) for model in models]
