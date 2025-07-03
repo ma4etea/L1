b3e2da6808088758d7c29a37b9ac486468313d17
ma4etea <alexvlod85@gmail.com>
2025-07-03 13:53:41 +0300

    задание 20

--- tests/integration_tests/booking/test_db.py
+++ tests/integration_tests/booking/test_db.py
@@ -1,9 +1,9 @@
 from datetime import date
 
-from src.schemas.booking import BookingToDB
+from src.schemas.booking import BookingToDB, Booking
 
 
-async def test_add_booking(db):
+async def test_crud_booking(db):
     users = await db.auth.get_all()
     rooms = await db.rooms.get_all()
 
@@ -15,4 +15,17 @@ async def test_add_booking(db):
         price=1500
     )
     new_model_data = await db.bookings.add(booking)
+    assert isinstance(new_model_data, Booking)
+
+    booking_get = await db.bookings.get_one_none(id=new_model_data.id)
+    assert isinstance(booking_get, Booking)
+
+    booking_edit = await db.bookings.edit(booking, id=new_model_data.id)
+    assert isinstance(booking_edit, Booking)
+
     await db.commit()
+
+    booking_delete = await db.bookings.delete(id=new_model_data.id)
+    assert booking_delete is None
+
+    await db.rollback()
