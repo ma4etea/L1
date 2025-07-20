7f34ddd29ce378beb31264ceb06fed23318d123d
ma4etea <alexvlod85@gmail.com>
2025-07-20 16:42:09 +0300

    Доработка

--- src/api/images.py
+++ src/api/images.py
@@ -1,16 +1,23 @@
 from pathlib import Path
 
-from fastapi import APIRouter, UploadFile, BackgroundTasks
+from fastapi import APIRouter, UploadFile, BackgroundTasks, HTTPException
 import shutil
-
-from fastapi.responses import JSONResponse
 
 from background_tasks.tasks import save_resized_images_f
 from src.services.image import ImageService
 
 router = APIRouter(prefix="/images", tags=["Загрузка изображений"])
 
-@router.post("")
+
+@router.post("",
+             summary="Загрузка изображения",
+             description=(
+                     "Загружает изображение, сохраняет его на диск и инициирует две параллельные обработки:\n\n"
+                     "1. `ImageService.save_resized_images(path)` — отправляет задачу в очередь Celery (отдельный процесс);\n"
+                     "2. `save_resized_images_f(path)` — запускается как фоновая задача FastAPI (в отдельном потоке).\n\n"
+                     "Обе обработки предназначены для создания уменьшенных копий изображения или других модификаций.\n\n"
+                     "Размер файла ограничен до 2 мегабайт (на уровне nginx)."
+             ))
 def upload_image(file: UploadFile, background_task: BackgroundTasks):
     # todo Нужно сделать интерфейс что бы передавать file в сервис
     path = f"src/static/images/{file.filename}"
@@ -26,16 +33,21 @@ def upload_image(file: UploadFile, background_task: BackgroundTasks):
     return {"status": "ok"}
 
 
-IMAGES_FOLDER = Path("src/static/images")
-
-@router.get("", summary="Список всех файлов в /images")
+@router.get("", summary="Список всех файлов в /images",
+            description=(
+                    "Возвращает список всех файлов, находящихся в директории `/images`, \n\n"
+                    "Если папка отсутствует или в ней нет файлов, возвращается ошибка 404."
+            ), )
 async def list_all_image_files():
-    if not IMAGES_FOLDER.exists():
-        return JSONResponse(status_code=404, content={"detail": "Папка не найдена"})
+    images_folder = Path("src/static/images")
+    if not images_folder.exists():
+        raise HTTPException(status_code=404, detail="Папка /images не найдена")
 
     all_files = [
-        str(p.relative_to(IMAGES_FOLDER))
-        for p in IMAGES_FOLDER.rglob("*")
+        str(p.relative_to(images_folder))
+        for p in images_folder.rglob("*")
         if p.is_file()
     ]
+    if not all_files:
+        raise HTTPException(status_code=404, detail="Файлы не найдены")
     return {"files": all_files}
