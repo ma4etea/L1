1af8c85864999504c4bc0f178c41d30ba270477c
ma4etea <alexvlod85@gmail.com>
2025-07-20 16:12:30 +0300

    Доработка

--- src/api/images.py
+++ src/api/images.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from fastapi import APIRouter, UploadFile, BackgroundTasks
 import shutil
+
+from fastapi.responses import JSONResponse
 
 from background_tasks.tasks import save_resized_images_f
 from src.services.image import ImageService
@@ -18,3 +22,20 @@ def upload_image(file: UploadFile, background_task: BackgroundTasks):
     background_task.add_task(
         save_resized_images_f, path
     )  # new_case: фоновая задачи fastapi, создает поток.
+
+    return {"status": "ok"}
+
+
+IMAGES_FOLDER = Path("src/static/images")
+
+@router.get("", summary="Список всех файлов в /images")
+async def list_all_image_files():
+    if not IMAGES_FOLDER.exists():
+        return JSONResponse(status_code=404, content={"detail": "Папка не найдена"})
+
+    all_files = [
+        str(p.relative_to(IMAGES_FOLDER))
+        for p in IMAGES_FOLDER.rglob("*")
+        if p.is_file()
+    ]
+    return {"files": all_files}
